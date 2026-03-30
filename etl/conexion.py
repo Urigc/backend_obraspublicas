50723e7edd2d2d13296bd20282652469d5091394
Uriel Gonzalez <gonzalezcasianouriel@gmail.com>
2026-03-30 02:23:37 -0600

    Update2 conexion.py

--- etl/conexion.py
+++ etl/conexion.py
@@ -5,10 +5,17 @@ import os
 load_dotenv()
 
 def get_conn():
+    db_url = os.getenv("DATABASE_URL")
+    
+    if db_url:
+        return psycopg2.connect(db_url)
+
+def get_conn():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT", 5432),
-        dbname=os.getenv("DB_NAME", "Obras_Publicas"),
+        dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        sslmode=os.getenv("DB_SSLMODE", "require") 
     )
